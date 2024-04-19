df0d67999dacfa009d42e744bd086921c60e708b
Angeles Guyot <angeles.guyot@kellogg.northwestern.edu>
2024-04-19 18:48:24 +0000

    Class 4

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,12 +5,23 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5 #significa que ponga el number 5 on the screen -- para correrlo pongo abajo: ruby code-along/1-data.rb
 
-# Non-numbers
+#put the sum of 2+5
+puts 2+5
+
+# Non-numbers - usar quotes - sin "" me aparece error
+
+puts "tacos"
+
 
 # True, False
 
-# Nothing
+puts true
+
+# Nothing - nil es nothing, no hace falta usar quotes
+
+puts nil
 
 # Variables
 
